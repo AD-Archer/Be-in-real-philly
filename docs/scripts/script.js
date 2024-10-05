9b10e35b2b1c1110f9212a85efbcf69d64b688f0
AD-Archer <adarcher21@gmail.com>
2024-10-05 12:11:38 -0400

    changes made to file structure

--- docs/scripts/script.js
+++
@@ -1,8 +0,0 @@
-var map = L.map('map').setView([39.9526, -75.1652], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map); 
-
-var marker = L.marker([39.9526, -75.1652]).addTo(map); //This adds markers to the map
